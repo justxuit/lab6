153d3f7ba03d22b104169e614c64b6bdc8057d96
Xu <email.xhuang@gmail.com>
2016-11-01 10:16:48 -0400

    Added temp converter function

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-
-
-var tempF=100;
-var tempC=0;
-
 //alert("Hi :)");
 
-console.log((5/9)*(tempF-32));
+//console.log((5/9)*(tempF-32));
 
-function convertTemp(tempF, tempC){
-
+function convertTemp(tempInput, tempUnit){
+  if (tempUnit==="C"){
+    return ((tempInput-32)*(5/9));
+  } else if (tempUnit==="F"){
+    return ((tempInput*9/5)+32);
+  } else {
+    console.log("Please select a valid temperature unit ('C' for Celsius, 'F' for Fahrenheit).")
+  }
 }
+
+var temperature = convertTemp(212, "C");
+
+console.log(temperature);
