e7a4ec55472c980ed9c9120a71168bb986d08b50
Xu <email.xhuang@gmail.com>
2016-11-08 11:40:31 -0500

    Added script2.js

---
+++ script2.js
@@ -0,0 +1,44 @@
+//alert("Hi :)");
+
+//console.log((5/9)*(tempF-32));
+/*
+function convertTemp(tempInput, tempUnit){
+  if (tempUnit==="C"){
+    return ((tempInput-32)*(5/9));
+  } else if (tempUnit==="F"){
+    return ((tempInput*9/5)+32);
+  } else {
+    return "Please select a valid temperature unit ('C' for Celsius, 'F' for Fahrenheit).";
+  }
+}
+*/
+
+/*
+console.log(convertTemp(212, "C"));
+console.log(convertTemp(32, "C"));
+console.log(convertTemp(65, "C"));
+console.log(convertTemp(-40, "F"));
+*/
+
+
+calculateButton.onclick = convertTemp;
+
+function convertTemp() {
+  var userTemp = parseFloat(document.getElementById("newTemp").value);
+  var userUnit = document.getElementById("newUnit").value;
+
+
+  if (userUnit!=="F"&&userUnit!=="C") {
+    alert("Please enter a valid unit of temperature. F for Fahrenheit, C for Celsius.");
+  }
+  else {
+    if (userUnit==="C"){
+      var answer = ((userTemp*9/5)+32) + " °F";
+    } else {
+      var answer = ((userTemp-32)*(5/9)) + " °C";
+    }
+    }
+
+  var outputValue = document.createElement("div");
+  document.getElementById("outputBox").innerHTML = answer;
+}
